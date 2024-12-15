fb0676222adf475bcad21d0e903d466c17d07912
Mohammed M. Abdel-Aziz <engmo7ammedma7moud@gmail.com>
2024-12-15 09:38:27 +0200

    adding routing

--- app.js
+++ app.js
@@ -15,3 +15,23 @@ const db_name = process.env.DB_NAME;
 app.listen(port, hostname, () => {
     console.log(`Server is running on port ${port} and hostname ${hostname} and database ${db_name}`);
 });
+
+//adding routes
+app.get('/', (req, res) => {
+    res.send('Hello World in NodeJS and Express!');
+});
+app.get('/test2', (req, res) => {
+    res.send('Hello World in NodeJS and Express!');
+});
+//adding routes to test request
+app.get('/test/:id', (req, res) => {
+    res.send(`request id ${req.params.id}`);
+});
+// adding routes to response json
+app.get('/test2/:id', (req, res) => {
+    return res.json({
+        id: req.params.id,
+        name: 'test2'
+    });
+});
+
